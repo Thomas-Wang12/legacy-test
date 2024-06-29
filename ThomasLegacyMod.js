439f10aa39323571e79bff0f8d78835d1ae4fcbe
Thomas Wang <thomas.wang0421@gmail.com>
2024-06-28 20:56:11 -0400

    Test

--- ThomasLegacyMod.js
+++ ThomasLegacyMod.js
@@ -366,7 +366,7 @@ G.AddData({
 					}
 					
 					if(G.has('sewing')) {
-						G.gain('happiness',-leftout*0.50,'no clothing');
+						G.gain('happiness',-leftout*0.15,'no clothing');
 					} else {
 						G.gain('happiness',-leftout*0.05,'no clothing');
 					}
@@ -387,7 +387,7 @@ G.AddData({
 					}
 
 					if(G.has('fire-making')) {
-						G.gain('happiness',-leftout*0.50,'cold & darkness');
+						G.gain('happiness',-leftout*0.15,'cold & darkness');
 					} else {
 						G.gain('happiness',-leftout*0.05,'cold & darkness');
 					}
